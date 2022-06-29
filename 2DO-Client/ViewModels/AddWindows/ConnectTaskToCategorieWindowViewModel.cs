95448ed980cf05030717a11e6bca56bd763f9870
Wochelele <Felix.Wochele@web.de>
2022-06-29 11:55:43 +0200

    Funktioniert immer noch 3

--- 2DO-Client/ViewModels/AddWindows/ConnectTaskToCategorieWindowViewModel.cs
+++ 2DO-Client/ViewModels/AddWindows/ConnectTaskToCategorieWindowViewModel.cs
@@ -1,14 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using _2DO_Client.Framework;
+using ServiceReference1;
+using Task = ServiceReference1.Task;
 
 namespace _2DO_Client.ViewModels.AddWindows
 {
-    internal class ConnectTaskToCategorieWindowViewModel
+    public class ConnectTaskToCategorieWindowViewModel : ViewModelBase
     {
 
+
+        public ICommand OkCommand { get; set; }
+
+        public ICommand CancelCommand { get; set; }
+
+        public ObservableCollection<ServiceReference1.Task> CategorieModels { get; set; } = new();
+
+        private Task mSelectedItem;
+
+        public Task SelectedItem
+        {
+            get
+            {
+                return mSelectedItem;
+            }
+            set
+            {
+                mSelectedItem = value;
+                OnPropertyChanged();
+            }
+        }
 
     }
 }
