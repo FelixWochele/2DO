aa97d0bc6d6adab191c6d58640e41e1ace5318e7
Wochelele <Felix.Wochele@web.de>
2022-06-29 12:20:44 +0200

    2

--- 2DO-Client/Controller/AddTaskWindowController/ConnectTaskToCategorieWindowController.cs
+++ 2DO-Client/Controller/AddTaskWindowController/ConnectTaskToCategorieWindowController.cs
@@ -1,20 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using _2DO_Client.ViewModels.AddWindows;
+using _2DO_Client.Views.AddTaskWindowViews;
 
 namespace _2DO_Client.Controller
 {
     public class ConnectTaskToCategorieWindowController
     {
 
+        private ConnectTaskToCategoryWindowView mView;
+        private ConnectTaskToCategorieWindowViewModel mViewModel;
+
         public ConnectTaskToCategorieWindowController()
         {
-
-
+            mViewModel = new();
+            mView = new();
 
         }
-
     }
 }
