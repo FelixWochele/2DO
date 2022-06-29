d1160fd355dddf0f54c169a4bd27460021550523
Wochelele <Felix.Wochele@web.de>
2022-06-29 11:48:44 +0200

    Funktioniert noch 1

---
+++ 2DO-Client/Views/AddTaskWindowViews/ConnectTaskToCategoryWindowView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _2DO_Client.Views.AddTaskWindowViews
+{
+    /// <summary>
+    /// Interaktionslogik für ConnectTaskToCategoryWindowView.xaml
+    /// </summary>
+    public partial class ConnectTaskToCategoryWindowView : Window
+    {
+        public ConnectTaskToCategoryWindowView()
+        {
+            InitializeComponent();
+        }
+    }
+}
