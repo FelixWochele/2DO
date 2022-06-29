daee048e0ef6a0b74e58f7f55fd0447ec15d766a
Wochelele <Felix.Wochele@web.de>
2022-06-29 12:49:58 +0200

    Test

--- 2DO-Client/Controller/AddTaskWindowController/ConnectTaskToCategorieWindowController.cs
+++ 2DO-Client/Controller/AddTaskWindowController/ConnectTaskToCategorieWindowController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using _2DO_Client.Framework;
 using _2DO_Client.ViewModels.AddWindows;
 using _2DO_Client.Views.AddTaskWindowViews;
 
@@ -20,6 +21,38 @@ namespace _2DO_Client.Controller
             mViewModel = new();
             mView = new();
 
+            mView.DataContext = mViewModel;
+
+            mViewModel.OkCommand = new RelayCommand(ExecuteOkCommand);
+
+            mViewModel.CancelCommand = new RelayCommand(ExecuteCancelCommand);
+
         }
+
+        private void ExecuteOkCommand(object obj)
+        {
+            mView.DialogResult = true;
+            mView.Close();
+        }
+
+        private void ExecuteCancelCommand(object obj)
+        {
+            mView.DialogResult = false;
+            mView.Close();
+        }
+
+
+        public ServiceReference1.Task Test()
+        {
+            var ret = mView.ShowDialog();
+
+            if (ret == true)
+            {
+                return mViewModel.SelectedItem;
+            }
+
+            return null;
+        }
+
     }
 }
